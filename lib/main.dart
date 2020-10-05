277dfe30f32470e98249f5db4ddf77c466596fc0
acasadomdam1 <acasadomdam1@gmail.com>
2020-10-05 22:00:50 +0200

    Text-To-Speech (TTS)

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_text_to_speech/flutter_text_to_speech.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Text To Speech',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: MyHomePage(title: 'Text To Speech'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  VoiceController _voiceController;
+
+  String text = 'Este texto prueba que la aplicación funciona correctamente';
+
+  @override
+  void initState() {
+    _voiceController = FlutterTextToSpeech.instance.voiceController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _voiceController.stop();
+  }
+
+  _playVoice() {
+    _voiceController.init().then((_) {
+      _voiceController.speak(
+        text,
+        VoiceControllerOptions(),
+      );
+    });
+  }
+
+  _stopVoice() {
+    _voiceController.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            RaisedButton(
+              onPressed: _playVoice,
+              color: Colors.lightBlue,
+              child: Text('Play Voice'),
+            ),
+            RaisedButton(
+              onPressed: _stopVoice,
+              color: Colors.red,
+              child: Text('Stop Voice'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
